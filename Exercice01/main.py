1f75abaf4939023bb7699232ce71175f9a8762f3
19maxi71 <131301678+19maxi71@users.noreply.github.com>
2024-10-31 13:00:22 +0100

    exercise01 faite

--- Exercice01/main.py
+++ Exercice01/main.py
@@ -1,2 +1,12 @@
 name = input("Entrez votre nom : ")
 age = int(input("Entrez votre âge : "))
+
+
+# Vous devez créer un programme Python qui affiche un message personnalisé en utilisant la méthode f-string. Le programme devra demander à l'utilisateur de saisir son nom et son âge, puis il affichera un message de salutation avec ces informations.
+ 
+# Le message final devra avoir exactement la forme suivante : "Bonjour, je m'appelle `<`nom`>` et j'ai `<`age`>` ans."
+
+def main():
+    print(f"Bonjour, je m'appelle {name} et j'ai {age} ans.")
+
+start= main()
