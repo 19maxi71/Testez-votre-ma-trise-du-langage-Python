392b85d007f0917646f65d20cb68e0a57d5742a0
19maxi71 <131301678+19maxi71@users.noreply.github.com>
2024-10-31 14:20:02 +0100

    ex05 faite

--- Exercice05/main.py
+++ Exercice05/main.py
@@ -1,5 +1,32 @@
+
 def sum(a, b):
+    """Calcule la somme de deux nombres.
+
+    Args:
+        a (int or float): premier nombre
+        b (int or float): deuxime nombre
+
+    Returns:
+        int of float: La somme de a et b
+    """
     return a + b
  
 def subtraction(a, b):
+    """Calcule la soustraction de deux nombres.
+
+    Args:
+        a (int or float): premier nombre
+        b (int or float): deuxime nombre
+
+    Returns:
+        int or float: Le résultat de a - b
+    """
     return a - b
+
+# # Instructions - Exercice 5
+
+# Vous avez reçu un fichier Python contenant plusieurs fonctions, mais aucune d'entre elles n'est documentée avec des docstrings. 
+
+# Votre tâche consiste à examiner le code et à ajouter des docstrings appropriées pour chaque fonction. 
+
+# Les docstrings doivent expliquer ce que fait chaque fonction, quels sont ses paramètres et ce qu'elle retourne. Assurez-vous d'utiliser le format de docstring conforme aux conventions PEP 257.
