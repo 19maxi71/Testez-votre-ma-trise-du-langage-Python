cf20591be9e925a3aa2e9e3aea8bd8a4f95d1a77
19maxi71 <131301678+19maxi71@users.noreply.github.com>
2024-10-31 13:26:21 +0100

    ex03 done

--- Exercice02/main.py
+++ Exercice02/main.py
@@ -1,3 +1,10 @@
+# Considérons un dictionnaire qui contient des informations sur des étudiants et leurs notes dans différentes matières.
+
+# Écrivez un programme Python qui effectue les actions suivantes :
+# - Demander à l'utilisateur de saisir le nom d'un étudiant avec le message suivant : “Entrez le nom de l’étudiant :  ”
+# - Afficher les notes de cet étudiant dans chaque matière “<nom de la matière> : <note>”, précédées par le message “Notes de <nom de l’étudiant> :  ”. Si l’étudiant n’existe pas, afficher exactement le message suivant : "L'étudiant <nom de l’étudiant> n'existe pas dans la liste."
+# - Calculer la moyenne des notes de cet étudiant et l'afficher comme suit : “Moyenne de <nom de l’étudiant> : <moyenne>”
+
 students = {
     'Alice': {
          'Mathematiques': 90,
@@ -15,3 +22,16 @@ students = {
          'Histoire': 78
      }
 }
+
+def main():
+    student_name = input("Entrez le nom de l’étudiant : ")
+    if student_name in students:
+        print(f"Notes de {student_name} : ")
+        for subject, grade in students[student_name].items():
+            print(f"{subject} : {grade}")
+        average = sum(students[student_name].values()) / len(students[student_name])
+        print(f"Moyenne de {student_name} : {average}")
+    else:
+        print(f"L'étudiant {student_name} n'existe pas dans la liste.")
+
+start= main()
--- Exercice03/main.py
+++ Exercice03/main.py
@@ -1,1 +1,13 @@
 words = ["python", "programmation", "langage", "ordinateur", "apprentissage"]
+
+# Considérez une liste de mots. Vous devez créer une liste de compréhension qui contient des tuples de chaque mot et le nombre de voyelles qu'il contient. À la fin de votre programme, affichez cette nouvelle liste créée à partir de la liste de compréhension.
+ 
+# Par exemple avec la liste suivante : ["openclassrooms", "developpeur", "algorithme"]
+# vous devrez avoir comme résultat, cette liste : [('openclassrooms', 5), ('developpeur', 5), ('algorithme', 4)]
+def main():
+    vowels = "aeiouAEIOUyY"
+    for word in words:
+        result = (word, len([letter for letter in word if letter in vowels]))
+        print(result)
+
+start = main()
